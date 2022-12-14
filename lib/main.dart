54f388a973ab808a147c67037fa34e3929d5243e
和田やまと <aubameyang@ip-10-79-13-77.ap-northeast-1.compute.internal>
2022-12-14 10:23:58 +0900

    少し改善

--- lib/main.dart
+++ lib/main.dart
@@ -7,32 +7,133 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage(title: 'Flutter Demo  '),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 72, 238, 50),
+        title: const Icon(Icons.sports_soccer),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            Container(
+              width: 350,
+              height: 200,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(''),
+                  )),
+            ),
             const Text(
-              'name yamto',
+              'My profile',
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    color: Color.fromARGB(255, 127, 124, 124),
+                    offset: Offset(5.0, 5.0),
+                    blurRadius: 4.0,
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+              'Name',
+              style: TextStyle(
+                color: Color.fromARGB(255, 72, 238, 50),
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    color: Color.fromARGB(255, 127, 124, 124),
+                    offset: Offset(5.0, 5.0),
+                    blurRadius: 4.0,
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+              'Wada Yamato',
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'Specialty',
+              style: TextStyle(
+                color: Color.fromARGB(255, 72, 238, 50),
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    color: Color.fromARGB(255, 127, 124, 124),
+                    offset: Offset(5.0, 5.0),
+                    blurRadius: 4.0,
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+              'Web Design',
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'Hobby',
+              style: TextStyle(
+                color: Color.fromARGB(255, 72, 238, 50),
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    color: Color.fromARGB(255, 127, 124, 124),
+                    offset: Offset(5.0, 5.0),
+                    blurRadius: 4.0,
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+              'Camp&Trip',
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
