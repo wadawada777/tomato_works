b257b53f6bdaff3fdc7d0f34bf07ada9b0434ef2
和田やまと <aubameyang@wadayamatonoMacBook-Air.local>
2022-12-14 14:01:19 +0900

    ページ遷移完了

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -3,10 +3,19 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class NextPage extends StatelessWidget {
-  const NextPage({super.key});
+  const NextPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var scaffold = Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        title: const Icon(Icons.sports_soccer),
+      ),
+      body: Center(
+        child: Text(""),
+      ),
+    );
+    return scaffold;
   }
 }
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var scaffold = Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        title: const Icon(Icons.sports_soccer),
+      ),
+      body: Center(
+        child: Text(""),
+      ),
+    );
+    return scaffold;
   }
 }
