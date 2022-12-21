823ab594cb56fea2cf807a9867e621a67cf580a8
和田やまと <aubameyang@wadayamatonoMacBook-Air.local>
2022-12-21 15:33:01 +0900

    背景追加

--- lib/main.dart
+++ lib/main.dart
@@ -41,123 +41,136 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Color.fromARGB(255, 19, 164, 0),
         title: const Icon(Icons.sports_soccer),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 200,
-              height: 150,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  alignment: Alignment.topCenter,
-                  fit: BoxFit.cover,
-                  image: AssetImage('images/IMG_3424.JPG'),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // ここに画像を入れる
+          Image.asset(
+            'images/haikei.JPG',
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 200,
+                  height: 150,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      alignment: Alignment.topCenter,
+                      fit: BoxFit.cover,
+                      image: AssetImage('images/IMG_3424.JPG'),
+                    ),
+                  ),
                 ),
-              ),
+                SizedBox(
+                  height: 1,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Name : Wada Yamato',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                const Text(
+                  'Profile',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                OutlinedButton(
+                  child: const Text('My Page'),
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    shape: const StadiumBorder(),
+                    side: const BorderSide(
+                        color: Color.fromARGB(255, 19, 164, 0)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ProfilePage()),
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Specialty',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                OutlinedButton(
+                  child: const Text('Web Site'),
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    shape: const StadiumBorder(),
+                    side: const BorderSide(
+                        color: Color.fromARGB(255, 19, 164, 0)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NextPage()),
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Hobby',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                OutlinedButton(
+                  child: const Text('My Hobby page'),
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    shape: const StadiumBorder(),
+                    side: const BorderSide(
+                        color: Color.fromARGB(255, 19, 164, 0)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HobbyPage()),
+                    );
+                  },
+                ),
+              ],
             ),
-            SizedBox(
-              height: 1,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'Name : Wada Yamato',
-              style: TextStyle(
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            const Text(
-              'Profile',
-              style: TextStyle(
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            OutlinedButton(
-              child: const Text('My Page'),
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.black,
-                shape: const StadiumBorder(),
-                side: const BorderSide(color: Color.fromARGB(255, 19, 164, 0)),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProfilePage()),
-                );
-              },
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            const Text(
-              'Specialty',
-              style: TextStyle(
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            OutlinedButton(
-              child: const Text('Web Site'),
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.black,
-                shape: const StadiumBorder(),
-                side: const BorderSide(color: Color.fromARGB(255, 19, 164, 0)),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => NextPage()),
-                );
-              },
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            const Text(
-              'Hobby',
-              style: TextStyle(
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            OutlinedButton(
-              child: const Text('My Hobby page'),
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.black,
-                shape: const StadiumBorder(),
-                side: const BorderSide(color: Color.fromARGB(255, 19, 164, 0)),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HobbyPage()),
-                );
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
